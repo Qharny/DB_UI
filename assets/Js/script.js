da7476b37e7d0b3aac3936f8a2eb1fc4d3467987
Qharny <cannycc1020@gmail.com>
2024-04-22 14:34:23 +0000

    modified html, css and Js file  added some files

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,6 +1,6 @@
 // the navbar scrolling effect.
 window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
+    const navbar = document.querySelector('.NavBar');
     const navbarHeight = navbar.getBoundingClientRect().height;
     const headerHeight = document.querySelector('header').getBoundingClientRect().height;
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -17,3 +17,29 @@ window.addEventListener('scroll', function() {
 });
 
 // the sliding effect in the catalogue section.
+document.addEventListener('DOMContentLoaded', function() {
+    new Swiper('.catalogue-slider', {
+        speed: 600,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+        }
+    });
+});
